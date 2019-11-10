ef97d5c5d59def2c8d930a0319f31d8db8b8040e
engineer1109 <1292846099@qq.com>
2019-11-10 23:16:27 +0800

    update code

--- cmake/Config.cmake
+++ cmake/Config.cmake
@@ -1,4 +1,8 @@
+if(UNIX)
+    option(USE_XCB "X11 xcb" ON)
+endif()
 option(GIT_ThirdParty "git submodule the thirdparty" ON)
 option(USE_OpenMP "Use CPU Multithread OpenMP" ON)
 option(BUILD_BASE "Build libopenglesbase.so" ON)
 option(BUILD_SimpleExamples "Build SimpleExamples" ON)
+
