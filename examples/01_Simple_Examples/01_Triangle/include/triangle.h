75a4cfe2ee35d56b14515a246f6b50e0624a1e00
engineer1109 <1292846099@qq.com>
2019-11-12 01:32:17 +0800

    update code

--- examples/01_Simple_Examples/01_Triangle/include/triangle.h
+++ examples/01_Simple_Examples/01_Triangle/include/triangle.h
@@ -1,11 +1,14 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
-#include "openglesbase.h"
-class Triangle:public OpenGLESBase{
+#include "opengles_basicengine.h"
+class Triangle:public OpenGLESBasicEngine{
 public:
     Triangle()=default;
     ~Triangle()=default;
 
     void render();
+
+    void keyPressed(uint32_t);
+    void mouseMoved(double x, double y, bool & handled);
 };
 #endif
--- examples/01_Simple_Examples/01_Triangle/src/triangle.cxx
+++ examples/01_Simple_Examples/01_Triangle/src/triangle.cxx
@@ -1,8 +1,24 @@
 #include <iostream>
 #include "triangle.h"
 void Triangle::render(){
-    std::cout<<"1"<<std::endl;
     glClearColor(1.0, 1.0, 0.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
     glFlush();
+    if(mouseWheel.up){
+        std::cout<<"up"<<std::endl;
+    }
+}
+
+void Triangle::keyPressed(uint32_t value){
+    std::cout<<value<<std::endl;
+}
+
+void Triangle::mouseMoved(double x, double y, bool & handled){
+    //std::cout<<x<<std::endl;
+    if(mouseButtons.left){
+        std::cout<<"click"<<std::endl;
+    }
+    if(mouseWheel.up){
+        std::cout<<"up"<<std::endl;
+    }
 }
