40306e0d706be68a0544ade52ca57942dc3e8520
engineer1109 <1292846099@qq.com>
2019-11-13 02:01:58 +0800

    update code

--- examples/01_Simple_Examples/01_Triangle/include/triangle.h
+++ examples/01_Simple_Examples/01_Triangle/include/triangle.h
@@ -1,14 +1,19 @@
 #ifndef TRIANGLE_H
 #define TRIANGLE_H
 #include "opengles_basicengine.h"
+class Shader;
 class Triangle:public OpenGLESBasicEngine{
 public:
     Triangle()=default;
     ~Triangle()=default;
 
+    void prepare();
     void render();
 
-    void keyPressed(uint32_t);
-    void mouseMoved(double x, double y, bool & handled);
+    //void keyPressed(uint32_t);
+    //void mouseMoved(double x, double y, bool & handled);
+private:
+    uint m_vertexArray=NULL;
+    Shader* m_shader=nullptr;
 };
 #endif
--- examples/01_Simple_Examples/01_Triangle/src/triangle.cxx
+++ examples/01_Simple_Examples/01_Triangle/src/triangle.cxx
@@ -1,24 +1,146 @@
 #include <iostream>
+#include "shader_m.h"
 #include "triangle.h"
+
+const char *vertexShaderSource ="#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "layout (location = 1) in vec3 aColor;\n"
+    "out vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos, 1.0);\n"
+    "   ourColor = aColor;\n"
+    "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "in vec3 ourColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(ourColor, 1.0f);\n"
+    "}\n\0";
+
+void Triangle::prepare(){
+    OpenGLESBase::prepare();
+    //glEnable(GL_DEPTH_TEST);
+//    m_shader= new Shader("../data/shaders/Simple_01_Triangle/01_Triangle/triangle.vert",
+//                     "../data/shaders/Simple_01_Triangle/01_Triangle/triangle.frag");
+//    m_shader->use();
+
+//    float vertices[] ={
+//        0.5f,0.0f,0.0f,1.0f,0.0f,0.0f,
+//       -0.5f,0.0f,0.0f,0.0f,1.0f,0.0f,
+//        0.0f,0.5f,0.0f,0.0f,0.0f,1.0f,
+//    };
+//    unsigned int VBO, m_vertexArray;
+//    glGenVertexArrays(1, &m_vertexArray);
+//    glGenBuffers(1, &VBO);
+
+//    glBindVertexArray(m_vertexArray);
+
+//    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+//    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+//    // position attribute
+//    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+//    glEnableVertexAttribArray(0);
+//    // texture coord attribute
+//    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+//    glEnableVertexAttribArray(1);
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+        glCompileShader(vertexShader);
+        // check for shader compile errors
+        int success;
+        char infoLog[512];
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+        // fragment shader
+        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+        glCompileShader(fragmentShader);
+        // check for shader compile errors
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+        }
+        // link shaders
+        int shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        // check for linking errors
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+        }
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+        // set up vertex data (and buffer(s)) and configure vertex attributes
+        // ------------------------------------------------------------------
+        float vertices[] = {
+            // positions         // colors
+             0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+             0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top
+
+        };
+
+        unsigned int VBO;
+        glGenVertexArrays(1, &m_vertexArray);
+        glGenBuffers(1, &VBO);
+        // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+        glBindVertexArray(m_vertexArray);
+
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+        // position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(0);
+        // color attribute
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
+
+        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+        // glBindVertexArray(0);
+
+        // as we only have a single shader, we could also just activate our shader once beforehand if we want to
+        glUseProgram(shaderProgram);
+
+}
+
 void Triangle::render(){
-    glClearColor(1.0, 1.0, 0.0, 1.0);
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
+    //glFlush();
+    //m_shader->use();
+    glBindVertexArray(m_vertexArray);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     if(mouseWheel.up){
         std::cout<<"up"<<std::endl;
     }
 }
 
-void Triangle::keyPressed(uint32_t value){
-    std::cout<<value<<std::endl;
-}
+//void Triangle::keyPressed(uint32_t value){
+//    std::cout<<value<<std::endl;
+//}
 
-void Triangle::mouseMoved(double x, double y, bool & handled){
-    //std::cout<<x<<std::endl;
-    if(mouseButtons.left){
-        std::cout<<"click"<<std::endl;
-    }
-    if(mouseWheel.up){
-        std::cout<<"up"<<std::endl;
-    }
-}
+//void Triangle::mouseMoved(double x, double y, bool & handled){
+//    //std::cout<<x<<std::endl;
+//    if(mouseButtons.left){
+//        std::cout<<"click"<<std::endl;
+//    }
+//    if(mouseWheel.up){
+//        std::cout<<"up"<<std::endl;
+//    }
+//}
