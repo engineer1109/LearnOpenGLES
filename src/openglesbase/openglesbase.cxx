f8af468fdb641cdcefa13f0a60ef35e7093b45a9
engineer1109 <1292846099@qq.com>
2019-11-16 22:37:18 +0800

    add win64 and linux64 both support

--- src/openglesbase/openglesbase.cxx
+++ src/openglesbase/openglesbase.cxx
@@ -6,7 +6,7 @@
 #include "opengles_imgui.h"
 #include "openglesbase.h"
 OpenGLESBase::OpenGLESBase(){
-    imgui=new ImguiOverlay();
+	imgui = new ImguiOverlay();
 #ifdef OPENGLES_USE_XCB
     initxcbConnection();
 #endif
@@ -23,36 +23,52 @@ void OpenGLESBase::initWindow(){
     setupWindow();
 }
 
-void OpenGLESBase::prepare(){
+void OpenGLESBase::prepareBase(){
     static EGLint const attribute_list[] = {
             EGL_RED_SIZE, 1,
             EGL_GREEN_SIZE, 1,
             EGL_BLUE_SIZE, 1,
             EGL_NONE
     };
+	EGLint contextAttribs[] = { EGL_CONTEXT_CLIENT_VERSION, 3, EGL_NONE, EGL_NONE };
+	EGLint majorVersion;
+	EGLint minorVersion;
 
     /* get an EGL display connection */
     display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
     /* initialize the EGL display connection */
-    eglInitialize(display, NULL, NULL);
+    eglInitialize(display, &majorVersion, &minorVersion);
     /* get an appropriate EGL frame buffer configuration */
     eglChooseConfig(display, attribute_list, &config, 1, &num_config);
     /* create an EGL rendering context */
     context = eglCreateContext(display, config, EGL_NO_CONTEXT, NULL);
     /* create a native window */
-    native_window = this->window;
+    native_window = (EGLNativeWindowType)this->window;
     /* create an EGL window surface */
     surface = eglCreateWindowSurface(display, config, native_window, NULL);
+
+	context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
+	if (context == EGL_NO_CONTEXT)
+	{
+		std::cout << "error eglCreateContext" << std::endl;
+	}
+
     /* connect the context to the surface */
     eglMakeCurrent(display, surface, surface, context);
+
+	printf("GL_RENDERER: %s\n", glGetString(GL_RENDERER));
+	printf("GL_VERSION: %s\n", glGetString(GL_VERSION));
+	printf("GL_SHADING_LANGUAGE_VERSION: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
     imgui->init();
 }
 
 void OpenGLESBase::renderLoop(){
+	destWidth = width;
+	destHeight = height;
+	lastTimestamp = std::chrono::high_resolution_clock::now();
 #ifdef OPENGLES_USE_XCB
     xcb_flush(connection);
-#endif
     while(!m_quit){
         auto tStart = std::chrono::high_resolution_clock::now();
         if (viewUpdated)
@@ -81,6 +97,67 @@ void OpenGLESBase::renderLoop(){
 
         eglSwapBuffers(display, surface);
     }
+#endif
+#ifdef OPENGLES_USE_WIN32
+	MSG msg;
+	bool quitMessageReceived = false;
+	while (!quitMessageReceived) {
+		while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+			if (msg.message == WM_QUIT) {
+				quitMessageReceived = true;
+				break;
+			}
+		}
+		if (!IsIconic(window)) {
+			auto tStart = std::chrono::high_resolution_clock::now();
+			if (viewUpdated)
+			{
+				viewUpdated = false;
+				viewChanged();
+			}
+
+			render();
+			frameCounter++;
+			auto tEnd = std::chrono::high_resolution_clock::now();
+			auto tDiff = std::chrono::duration<double, std::milli>(tEnd - tStart).count();
+			frameTimer = (float)tDiff / 1000.0f;
+			camera.update(frameTimer);
+			if (camera.moving())
+			{
+				viewUpdated = true;
+			}
+			if (settings.overlay) {
+				imgui->render();
+			}
+
+			eglSwapBuffers(display, surface);
+			// Convert to clamped timer value
+			if (!paused)
+			{
+				timer += timerSpeed * frameTimer;
+				if (timer > 1.0)
+				{
+					timer -= 1.0f;
+				}
+			}
+			float fpsTimer = (float)(std::chrono::duration<double, std::milli>(tEnd - lastTimestamp).count());
+			if (fpsTimer > 1000.0f)
+			{
+				lastFPS = static_cast<uint32_t>((float)frameCounter * (1000.0f / fpsTimer));
+#if defined(_WIN32)
+				if (!settings.overlay) {
+					std::string windowTitle = getWindowTitle();
+					SetWindowText(window, windowTitle.c_str());
+				}
+#endif
+				frameCounter = 0;
+				lastTimestamp = tEnd;
+			}
+		}
+	}
+#endif
 }
 
 void OpenGLESBase::render(){
@@ -94,10 +171,10 @@ void OpenGLESBase::handleMouseMove(int32_t x, int32_t y)
 
     bool handled = false;
 
-//    if (settings.overlay) {
-//        ImGuiIO& io = ImGui::GetIO();
-//        handled = io.WantCaptureMouse;
-//    }
+    if (settings.overlay) {
+        ImGuiIO& io = ImGui::GetIO();
+        handled = io.WantCaptureMouse;
+    }
     mouseMoved((float)x, (float)y, handled);
 
     if (handled) {
@@ -333,5 +410,265 @@ void OpenGLESBase::handleEvent(const xcb_generic_event_t *event)
     default:
         break;
     }
+}
+#endif
+
+
+#ifdef OPENGLES_USE_WIN32
+OpenGLESBase *g_opengles;                                                                        
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)                        
+{                                                                                                    
+	if (g_opengles != NULL)
+	{
+		g_opengles->handleMessages(hWnd, uMsg, wParam, lParam);
+	}
+	return (DefWindowProc(hWnd, uMsg, wParam, lParam));
+}
+
+
+void OpenGLESBase::setupWindow() {
+	WNDPROC wndproc = WndProc;
+	HINSTANCE hinstance = ::GetModuleHandle(NULL);
+	g_opengles = this;
+	this->windowInstance = hinstance;
+
+	WNDCLASSEX wndClass;
+
+	wndClass.cbSize = sizeof(WNDCLASSEX);
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;
+	wndClass.lpfnWndProc = wndproc;
+	wndClass.cbClsExtra = 0;
+	wndClass.cbWndExtra = 0;
+	wndClass.hInstance = hinstance;
+	wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = name.c_str();
+	wndClass.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
+
+	if (!RegisterClassEx(&wndClass))
+	{
+		std::cout << "Could not register window class!\n";
+		fflush(stdout);
+		exit(1);
+	}
+
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	if (settings.fullscreen)
+	{
+		DEVMODE dmScreenSettings;
+		memset(&dmScreenSettings, 0, sizeof(dmScreenSettings));
+		dmScreenSettings.dmSize = sizeof(dmScreenSettings);
+		dmScreenSettings.dmPelsWidth = screenWidth;
+		dmScreenSettings.dmPelsHeight = screenHeight;
+		dmScreenSettings.dmBitsPerPel = 32;
+		dmScreenSettings.dmFields = DM_BITSPERPEL | DM_PELSWIDTH | DM_PELSHEIGHT;
+
+		if ((width != (uint32_t)screenWidth) && (height != (uint32_t)screenHeight))
+		{
+			if (ChangeDisplaySettings(&dmScreenSettings, CDS_FULLSCREEN) != DISP_CHANGE_SUCCESSFUL)
+			{
+				if (MessageBox(NULL, "Fullscreen Mode not supported!\n Switch to window mode?", "Error", MB_YESNO | MB_ICONEXCLAMATION) == IDYES)
+				{
+					settings.fullscreen = false;
+				}
+				else
+				{
+					window = nullptr;
+					return;
+				}
+			}
+		}
+
+	}
+
+	DWORD dwExStyle;
+	DWORD dwStyle;
+
+	if (settings.fullscreen)
+	{
+		dwExStyle = WS_EX_APPWINDOW;
+		dwStyle = WS_POPUP | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+	}
+	else
+	{
+		if (enableWindow) {
+			dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
+			dwStyle = WS_OVERLAPPEDWINDOW | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+		}
+		else {
+			dwExStyle = WS_EX_APPWINDOW;
+			dwStyle = WS_POPUP | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+		}
+	}
+
+	RECT windowRect;
+	windowRect.left = 0L;
+	windowRect.top = 0L;
+	windowRect.right = settings.fullscreen ? (long)screenWidth : (long)width;
+	windowRect.bottom = settings.fullscreen ? (long)screenHeight : (long)height;
+
+	AdjustWindowRectEx(&windowRect, dwStyle, FALSE, dwExStyle);
+
+	std::string windowTitle = getWindowTitle();
+	window = CreateWindowEx(0,
+		name.c_str(),
+		windowTitle.c_str(),
+		dwStyle | WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+		0,
+		0,
+		windowRect.right - windowRect.left,
+		windowRect.bottom - windowRect.top,
+		NULL,
+		NULL,
+		hinstance,
+		NULL);
+
+	if (!settings.fullscreen)
+	{
+		// Center on screen
+		uint32_t x = (GetSystemMetrics(SM_CXSCREEN) - windowRect.right) / 2;
+		uint32_t y = (GetSystemMetrics(SM_CYSCREEN) - windowRect.bottom) / 2;
+		SetWindowPos(window, 0, x, y, 0, 0, SWP_NOZORDER | SWP_NOSIZE);
+	}
+
+	if (!window)
+	{
+		printf("Could not create window!\n");
+		fflush(stdout);
+		window = nullptr;
+		return;
+		exit(1);
+	}
+
+	ShowWindow(window, SW_SHOW);
+	SetForegroundWindow(window);
+	SetFocus(window);
+}
+
+void OpenGLESBase::handleMessages(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CLOSE:
+		prepared = false;
+		DestroyWindow(hWnd);
+		PostQuitMessage(0);
+		break;
+	case WM_PAINT:
+		ValidateRect(window, NULL);
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case KEY_P:
+			paused = !paused;
+			break;
+		case KEY_F1:
+			if (settings.overlay) {
+				//UIOverlay.visible = !UIOverlay.visible;
+			}
+			break;
+		case KEY_ESCAPE:
+			PostQuitMessage(0);
+			break;
+		}
+
+		if (camera.firstperson)
+		{
+			switch (wParam)
+			{
+			case KEY_W:
+				camera.keys.up = true;
+				break;
+			case KEY_S:
+				camera.keys.down = true;
+				break;
+			case KEY_A:
+				camera.keys.left = true;
+				break;
+			case KEY_D:
+				camera.keys.right = true;
+				break;
+			}
+		}
+
+		keyPressed((uint32_t)wParam);
+		break;
+	case WM_KEYUP:
+		if (camera.firstperson)
+		{
+			switch (wParam)
+			{
+			case KEY_W:
+				camera.keys.up = false;
+				break;
+			case KEY_S:
+				camera.keys.down = false;
+				break;
+			case KEY_A:
+				camera.keys.left = false;
+				break;
+			case KEY_D:
+				camera.keys.right = false;
+				break;
+			}
+		}
+		break;
+	case WM_LBUTTONDOWN:
+		mousePos = glm::vec2((float)LOWORD(lParam), (float)HIWORD(lParam));
+		mouseButtons.left = true;
+		break;
+	case WM_RBUTTONDOWN:
+		mousePos = glm::vec2((float)LOWORD(lParam), (float)HIWORD(lParam));
+		mouseButtons.right = true;
+		break;
+	case WM_MBUTTONDOWN:
+		mousePos = glm::vec2((float)LOWORD(lParam), (float)HIWORD(lParam));
+		mouseButtons.middle = true;
+		break;
+	case WM_LBUTTONUP:
+		mouseButtons.left = false;
+		break;
+	case WM_RBUTTONUP:
+		mouseButtons.right = false;
+		break;
+	case WM_MBUTTONUP:
+		mouseButtons.middle = false;
+		break;
+	case WM_MOUSEWHEEL:
+	{
+		short wheelDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+		zoom += (float)wheelDelta * 0.005f * zoomSpeed;
+		camera.translate(glm::vec3(0.0f, 0.0f, (float)wheelDelta * 0.005f * zoomSpeed));
+		viewUpdated = true;
+		break;
+	}
+	case WM_MOUSEMOVE:
+	{
+		handleMouseMove(LOWORD(lParam), HIWORD(lParam));
+		break;
+	}
+	case WM_SIZE:
+		if ((prepared) && (wParam != SIZE_MINIMIZED))
+		{
+			if ((resizing) || ((wParam == SIZE_MAXIMIZED) || (wParam == SIZE_RESTORED)))
+			{
+				destWidth = LOWORD(lParam);
+				destHeight = HIWORD(lParam);
+				windowResize();
+			}
+		}
+		break;
+	case WM_ENTERSIZEMOVE:
+		resizing = true;
+		break;
+	case WM_EXITSIZEMOVE:
+		resizing = false;
+		break;
+	}
 }
 #endif
