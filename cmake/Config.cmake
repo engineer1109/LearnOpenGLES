6d6d813cb27bd64bcd0f8da9f688b9d854921f43
engineer1109 <1292846099@qq.com>
2019-12-15 21:16:21 +0800

    add qt

--- cmake/Config.cmake
+++ cmake/Config.cmake
@@ -7,4 +7,5 @@ option(GIT_ThirdParty "git submodule the thirdparty" ON)
 option(USE_OpenMP "Use CPU Multithread OpenMP" ON)
 option(BUILD_BASE "Build libopenglesbase.so" ON)
 option(BUILD_SimpleExamples "Build SimpleExamples" ON)
+option(BUILD_QtExamples "Build SimpleExamples" OFF)
 
