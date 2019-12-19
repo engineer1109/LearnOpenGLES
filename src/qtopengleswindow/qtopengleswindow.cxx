17568aab7bc1d79bbfa52c8d94f1a53f56384370
wjl <1292846099@qq.com>
2019-12-19 18:16:11 +0800

    update qt

---
+++ src/qtopengleswindow/qtopengleswindow.cxx
@@ -0,0 +1,8 @@
+#include "qtopengleswindow.h"
+QtOpenGLESWindow::QtOpenGLESWindow(QWidget *parent):QMainWindow (parent){
+    this->setMouseTracking(true);
+}
+
+QtOpenGLESWindow::~QtOpenGLESWindow(){
+
+}
