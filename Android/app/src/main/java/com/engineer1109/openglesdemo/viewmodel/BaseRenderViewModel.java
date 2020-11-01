13e4af44555a7fa9130c1e6392b775943db47262
engineer1109 <1292846099@qq.com>
2020-11-01 23:28:36 +0800

    Surface TouchEvent

--- Android/app/src/main/java/com/engineer1109/openglesdemo/viewmodel/BaseRenderViewModel.java
+++ Android/app/src/main/java/com/engineer1109/openglesdemo/viewmodel/BaseRenderViewModel.java
@@ -10,6 +10,8 @@ import com.engineer1109.openglesdemo.render.BaseRender;
 public class BaseRenderViewModel extends ViewModel {
     private BaseRender mRender = new BaseRender();
 
+    private int mTouchMode = 0;
+
     public void init(int id) {
         mRender.init(id);
     }
@@ -25,4 +27,16 @@ public class BaseRenderViewModel extends ViewModel {
     public void render() {
         mRender.render();
     }
+
+    public void setTouchPos(float x, float y){}
+
+    public void setTouchPosSecond(float x, float y){}
+
+    public void setTouchMode(int mode){ mTouchMode = mode;}
+
+    public int getTouchMode() {return mTouchMode;}
+
+    public void resetTouch(){}
+
+    public int getRenderStatus() {return 1;}
 }
