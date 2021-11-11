e0f55f6fcabc9e91e87211df343fe471b97c6a54
engineer <1292846099@qq.com>
2021-11-11 23:24:14 +0800

    Update openglesbase.h

--- include/openglesbase/openglesbase.h
+++ include/openglesbase/openglesbase.h
@@ -1,3 +1,10 @@
+/*
+* LearnVulkan Examples
+*
+* Copyright (C) by engineer1109 - https://github.com/engineer1109/LearnOpenGLES
+*
+* This code is licensed under GNU General Public License v3.0  (GPL-3.0) (https://opensource.org/licenses/GPL-3.0)
+*/
 #ifndef OPENGLESBASE_H
 #define OPENGLESBASE_H
 
